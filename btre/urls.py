07cb72268f3d9a4727eabc50490c98d4e3fc09be
Anil Gupta <anilanilguptaa@gmail.com>
2020-07-05 20:21:32 +0530

    added some templates

--- btre/urls.py
+++ btre/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from pages import urls
+from pages import urls , views
 
 urlpatterns = [
+    path('',views.index, name='index'),
     path('admin/', admin.site.urls),
     path('',include('pages.urls'))
 ]
