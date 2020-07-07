c6c4d10afebfb17276fc73978e55dc64d0698436
Anil Gupta <anilanilguptaa@gmail.com>
2020-07-07 21:42:20 +0530

    added templates and edited

--- btre/settings.py
+++ btre/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMP_DIR = os.path.join(BASE_DIR, 'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -121,10 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'btre/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static'),
 ]
